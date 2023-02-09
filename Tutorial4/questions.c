898045abfbd40f40216ab77d4e033c82d9b981c6
Ahmaad Ansari <ahmaad.ali.ansari@hotmail.com>
2023-02-09 20:48:30 +0000

    Commit

--- Tutorial4/questions.c
+++ Tutorial4/questions.c
@@ -14,7 +14,68 @@
 // Initializes the array of questions for the game
 void initialize_game(void)
 {
+    printf("Welcome to the C Command Line version of Jeopardy!\n\n");
+    printf("In this game, you will be given a category and a clue. Your task is to provide the correct answer.\n");
+    printf("The value of each question will be determined by its difficulty. The more difficult the question, the higher the value.\n");
+    printf("Let's get started! Good luck and have fun!\n\n");
+
+    char *categories[] = {
+        "History", 
+        "Literature", 
+        "Science and Technology"
+    }
+    char *questions[] = {
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+    }
+    char *answers[] = {
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+    }
+    int values[] = {
+        100,
+        200,
+        300,
+        400
+    }
+
     // initialize each question struct and assign it to the questions array
+    for (int i = 0; i < 12; i++) {
+        strcpy(questions[i].category, "");
+        strcpy(questions[i].question, "");
+        strcpy(questions[i].answer, "");
+        strcpy(questions[i].value, "");
+        strcpy(questions[i].answered, FALSE);
+
+        char category[MAX_LEN];
+        char question[MAX_LEN];
+        char answer[MAX_LEN];
+        int value;
+        bool answered;
+    }
+
+
+
 }
 
 // Displays each of the remaining categories and question dollar values that have not been answered
